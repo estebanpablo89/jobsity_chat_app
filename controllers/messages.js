f0f80f8849bd25c1a0d16f044424b2e1c47ffd3d
estebanpablo89 <estebanpablo89@gmail.com>
2021-03-22 17:05:52 -0500

    message emit

--- controllers/messages.js
+++ controllers/messages.js
@@ -3,7 +3,9 @@ const Message = require('../models/Message');
 exports.getMessages = (req, res) => {
   Message.find({}, (err, messages) => {
     res.send(messages);
-  }).sort([['date', -1]]);
+  })
+    .sort([['date', -1]])
+    .limit(50);
 };
 
 exports.createMessage = (req, res) => {
